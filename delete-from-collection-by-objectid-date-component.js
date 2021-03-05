f62eb0f4b0646f5f980d3fcd52b6fbce512d422d
st04 <seantech@gmail.com>
2021-03-05 11:07:33 +0100

    Support dates down to 1970-01-01
    An object-ids date part must always be 4 bytes.

--- delete-from-collection-by-objectid-date-component.js
+++ delete-from-collection-by-objectid-date-component.js
@@ -1,4 +1,13 @@
-var dateToObjectId = date => ObjectId(Math.floor(date/1000).toString(16) + "0000000000000000");
+var dateToObjectId = date => {    
+    var datePart = Math.floor(date/1000).toString(16);
+    
+    // Add support for dates down to 1970-01-01 (Unix epoch)
+    // The datePart MUST always represent 4 bytes.
+    while(datePart.length < 8) 
+        datePart = '0' + datePart;
+        
+    return ObjectId(datePart + "0000000000000000");
+}
 
 var allDocumentsCreatedBefore = date => {
     return {_id : {$lt: dateToObjectId(date)}};
