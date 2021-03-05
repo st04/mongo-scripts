ec7848f6d6cc85695ae3d49c2fc50648da41b521
st04 <seantech@gmail.com>
2021-03-05 11:04:04 +0100

    Support dates down to 1970-01-01
    An object-ids date part must always be 4 bytes.

--- collection-growth-per-period.js
+++ collection-growth-per-period.js
@@ -1,5 +1,14 @@
 var byteToGigs = bytes => bytes / 1073741824;
-var dateToObjectId = date => ObjectId(Math.floor(date/1000).toString(16) + "0000000000000000");
+var dateToObjectId = date => {    
+    var datePart = Math.floor(date/1000).toString(16);
+    
+    // Add support for dates down to 1970-01-01 (Unix epoch)
+    // The datePart MUST always represent 4 bytes.
+    while(datePart.length < 8) 
+        datePart = '0' + datePart;
+        
+    return ObjectId(datePart + "0000000000000000");
+}
 
 var countItemsCreated = function(collection, startDate, endDate) {
     var startId = dateToObjectId(startDate);
